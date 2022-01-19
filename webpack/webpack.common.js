3dd8bbd130a0e4ab69229ca1414fd9c1e99dc0fa
Pike96 <linyucheng256@gmail.com>
2022-01-18 19:41:50 -0800

    Reactify - Remove content script

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,6 @@ module.exports = {
       popup: path.join(srcDir, 'popup.tsx'),
       options: path.join(srcDir, 'options.tsx'),
       background: path.join(srcDir, 'background.ts'),
-      content_script: path.join(srcDir, 'content_script.tsx'),
     },
     output: {
         path: path.join(__dirname, "../dist/js"),
